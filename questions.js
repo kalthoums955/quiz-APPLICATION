68406b1a15aded0a13347e1d6428a477ffb7dbd4
kalthoums955 <110736893+kalthoums955@users.noreply.github.com>
2022-12-16 20:57:07 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which nation was the first Asian country to reach the World Cup semi-finals?",
+    answer: "South Korea",
+    options: [
+      "South Korea",
+      "India",
+      "Japan",
+      "china"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many teams qualify from the group stages to the knockout rounds?",
+    answer: "16",
+    options: [
+      "4",
+      "8",
+      "16",
+      "24"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In what year was the World Cup first broadcast on TV?",
+    answer: "1954",
+    options: [
+      "1948",
+      "1954",
+      "1962",
+      "1975"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who won the 2018 FIFA World Cup?",
+    answer: "FRANCE",
+    options: [
+      "ENGLAND",
+      "Germany",
+      "FRANCE",
+      "BREZIL" ,
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many times have Italy won the World Cup so far?",
+    answer: "Four times",
+    options: [
+      "Twice",
+      "Four times",
+      "Six times",
+      "Eight times"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
